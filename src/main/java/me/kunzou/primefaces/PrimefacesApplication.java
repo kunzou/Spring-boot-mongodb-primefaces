0072e076b6b7c5fbff18e6b88a2717d32b66a7d2
kunzou@gmail.com <kunzou@gmail.com>
2022-03-01 13:45:45 -0800

    Springboot skeleton

---
+++ src/main/java/me/kunzou/primefaces/PrimefacesApplication.java
@@ -0,0 +1,13 @@
+package me.kunzou.primefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimefacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimefacesApplication.class, args);
+	}
+
+}
